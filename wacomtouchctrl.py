2b6485e35ca936d0eb60be2186a2fb145623d7aa
Ty Phillips <typhillips73@gmail.com>
2018-09-02 18:18:11 -0500

    Added some comments.

--- wacomtouchctrl.py
+++ wacomtouchctrl.py
@@ -11,19 +11,21 @@
 from builtins import str
 import subprocess
 
+# List devices known to Wacom driver
 sp_args = ["xsetwacom", "--list"]
 info = str(subprocess.check_output(sp_args), 'ascii').splitlines(True)
 
 found = False
 
 for line in info:
+	# Do some rudimentary checks on the output to make sure the required info is contained within
 	if line.find("touch") > 0 and line.find("id:") > 0:
 		found = True
-		id = line.split("id:")[1].split()[0]
+		id = line.split("id:")[1].split()[0]				# Get the touch ID
 
-if found:
-	sp_args = ["xsetwacom", "--set", id, "Touch", "off"]
+if found:													# Output was valid
+	sp_args = ["xsetwacom", "--set", id, "Touch", "off"]	# Set 'Touch' parameter to 'off' for detected ID
 	subprocess.call(sp_args)
 	print("Touch disabled (id = %s)" % id)
-else:
+else:														# Output was bad
 	print("Error! Confirm tablet is connected and \'xsetwacom\' is installed")
